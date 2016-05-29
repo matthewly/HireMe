9aaa5f23a9161d076fc0dc608a6701f054212759
matthewly <mmly2@illinois.edu>
2016-05-29 16:51:08 -0500

    updated dependencies file

--- client-side/source_js/controllers.js
+++ client-side/source_js/controllers.js
@@ -19,22 +19,25 @@ HireMeControllers.controller('searchController', ['$scope', '$http', '$cookies',
 }]);
 
 HireMeControllers.controller('internshipController', ['$scope', '$http', '$cookies', function($scope, $http, $cookies) {
+	$scope.overall_rating = "test";
+
 	$http.get('data/internships.json').success(function(data) {
 		$scope.items = data.items;
 
 		// for (var i = 0; i < items.length; i++) {
 		// 	if (items[i].displayLink ==)
 		// }
+
+		$http.jsonp('http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=69178&t.k=k0TM4AtrHme&action=employers&userip=88.192.249.8&useragent=Mozilla/%2F5.0"&q=riot games&callback=JSON_CALLBACK').success(function(data) {
+			console.log(data);
+			$scope.overall_rating = data.response.employers[0].overallRating;
+			console.log($scope.overall_rating);
+		}).error(function(err) {
+			console.log(err);
+		});
 	
 	}).error(function (err) {
 		console.log(err);
 	});
 
-	$http.jsonp('http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=69178&t.k=k0TM4AtrHme&action=employers&userip=88.192.249.8&useragent=Mozilla/%2F5.0"&q=riot games&callback=JSON_CALLBACK').success(function(data) {
-		console.log(data);
-		$scope.overall_rating = data.response.employers[0].overallRating;
-		console.log($scope.overall_rating);
-	}).error(function(err) {
-		console.log(err);
-	});
 }]);
