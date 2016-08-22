73bdcb9a2166809204f0a8628f2dc0a886bc8a63
matthewly <mmly2@illinois.edu>
2016-08-21 21:07:20 -0700

    fixed UI bug and renamed package

--- client/source_js/services.js
+++ client/source_js/services.js
@@ -2,7 +2,7 @@ var HireMeServices = angular.module('HireMeServices', []);
 
 HireMeServices.factory('Jobs', function($http, $window) {
 
-var baseUrl = 'http://localhost:4000';//'http://104.236.106.65:4000';
+var baseUrl = 'http://localhost:4000';//'http://107.170.217.181:4000';
     return {
         updateList : function(data) {
           return $http.post(baseUrl+'/api/update', data);
